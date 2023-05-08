7fdd374267d7837532df10116769f59d9db2a39f
Technicalhumair <104243587+Technicalhumair@users.noreply.github.com>
2023-05-08 13:09:29 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,115 +4,97 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const endpoint = "https://alcash.000webhostapp.com/graphql";
-  const graphQLClient = new GraphQLClient(endpoint);
-  const referringURL = ctx.req.headers?.referer || null;
-  const pathArr = ctx.query.postpath as Array<string>;
-  const path = pathArr.join('/');
-  console.log(path);
-  const fbclid = ctx.query.fbclid;
+	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const graphQLClient = new GraphQLClient(endpoint);
+	const referringURL = ctx.req.headers?.referer || null;
+	const pathArr = ctx.query.postpath as Array<string>;
+	const path = pathArr.join('/');
+	console.log(path);
+	const fbclid = ctx.query.fbclid;
 
-  // Redirect if Facebook is the referrer or request contains fbclid
-  if (referringURL?.includes('facebook.com') || fbclid) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: 'https://www.highrevenuegate.com/ppcjyq1g?key=32e0ca0c0da29351e9fb869a1c07a5e8/',
-      },
-    };
-  }
+	// redirect if facebook is the referer or request contains fbclid
+		if (referringURL?.includes('facebook.com') || fbclid) {
 
-  // Redirect if WhatsApp is the referrer
-  if (referringURL?.includes('whatsapp.com')) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: 'https://www.highrevenuegate.com/custom-link-for-whatsapp/',
-      },
-    };
-  }
+		return {
+			redirect: {
+				permanent: false,
+				destination: `${
+					`https://alcash.000webhostapp.com/` + encodeURI(path as string)
+				}`,
+			},
+		};
+		}
+	const query = gql`
+		{
+			post(id: "/${path}/", idType: URI) {
+				id
+				excerpt
+				title
+				link
+				dateGmt
+				modifiedGmt
+				content
+				author {
+					node {
+						name
+					}
+				}
+				featuredImage {
+					node {
+						sourceUrl
+						altText
+					}
+				}
+			}
+		}
+	`;
 
-  if (referringURL?.includes('pinterest.com') || pin) {
-    return {
-      redirect: {
-        permanent: false,
-        destination: 'https://www.highrevenuegate.com/ppcjyq1g?key=32e0ca0c0da29351e9fb869a1c07a5e8/',
-      },
-    };
-  }
-
-  const query = gql`
-    {
-      post(id: "/${path}/", idType: URI) {
-        id
-        excerpt
-        title
-        link
-        dateGmt
-        modifiedGmt
-        content
-        author {
-          node {
-            name
-          }
-        }
-        featuredImage {
-          node {
-            sourceUrl
-            altText
-          }
-        }
-      }
-    }
-  `;
-
-  const data = await graphQLClient.request(query);
-  if (!data.post) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      path,
-      post: data.post,
-      host: ctx.req.headers.host,
-    },
-  };
+	const data = await graphQLClient.request(query);
+	if (!data.post) {
+		return {
+			notFound: true,
+		};
+	}
+	return {
+		props: {
+			path,
+			post: data.post,
+			host: ctx.req.headers.host,
+		},
+	};
 };
 
 interface PostProps {
-  post: any;
-  host: string;
-  path: string;
+	post: any;
+	host: string;
+	path: string;
 }
 
 const Post: React.FC<PostProps> = (props) => {
-  const { post, host, path } = props;
+	const { post, host, path } = props;
 
-  // To remove tags from excerpt
-  const removeTags = (str: string) => {
-    if (str === null || str === '') return '';
-    else str = str.toString();
-    return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-  };
+	// to remove tags from excerpt
+	const removeTags = (str: string) => {
+		if (str === null || str === '') return '';
+		else str = str.toString();
+		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
+	};
 
-  return (
-    <>
-      <Head>
-        <meta property="og:title" content={post.title} />
-        <meta property="og:description" content={removeTags(post.excerpt)} />
-        <meta property="og:type" content="article" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content={host.split('.')[0]} />
-        <meta property="article:published_time" content={post.dateGmt}
-	<meta property="article:modified_time" content={post.modifiedGmt} />
-	<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-	<meta 
-		property="og:image:alt" content={post.featuredImage.node.altText || post.title}
-		
-		/>
-				
+	return (
+		<>
+			<Head>
+				<meta property="og:title" content={post.title} />
+				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:type" content="article" />
+				<meta property="og:locale" content="en_US" />
+				<meta property="og:site_name" content={host.split('.')[0]} />
+				<meta property="article:published_time" content={post.dateGmt} />
+				<meta property="article:modified_time" content={post.modifiedGmt} />
+				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta
+					property="og:image:alt"
+					content={post.featuredImage.node.altText || post.title}
+				/>
 				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">

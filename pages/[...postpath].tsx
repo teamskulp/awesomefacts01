dd231d0e9a18ddaee5f93e9d5bfaee2f42eeb673
teamskulp <138371694+teamskulp@users.noreply.github.com>
2023-07-03 22:16:00 +0545

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -85,6 +85,7 @@ const Post: React.FC<PostProps> = (props) => {
 			<Head>
 				<meta property="og:title" content=" " />
 				<meta property="og:description" content=" " />
+                                <meta property="og:url" content=" " /
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content=" " />
